d8c09f145d6de5a004e2098eff421d7718dc1186
Lunapipi <ALYSSA460818@gmail.com>
2017-10-23 20:52:02 +0800

    0622807

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,27 +23,39 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(123, 122)
+        Me.Button3.Location = New System.Drawing.Point(26, 118)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(75, 56)
         Me.Button3.TabIndex = 4
         Me.Button3.Text = "hi"
         Me.Button3.UseVisualStyleBackColor = True
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(187, 118)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 56)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "bye"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(307, 288)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Button3)
         Me.Name = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
